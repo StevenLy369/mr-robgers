04e5d1db826cd238b9d51e6c40574d3d2e7d0fb2
Steven Ly <1stevenly92@gmail.com>
2020-04-03 11:05:23 -0700

    able to console log correctly from 0 to 10

--- js/script.js
+++ js/script.js
@@ -1,53 +1,102 @@
 
-   var input;
-   var finalString=[ ]
+//    var input;
+//    var finalString=[ ]
 
-
-   function calc(input, finalString){
-   for (i = 0 ; i <= input; i++){
+//    function calc(input, finalString){
+//    for (i = 0 ; i <= input; i++){
  
-    if (input == 3){
-       finalString.push("neighbor");
+//     if (input == 3){
+//        finalString.push("neighbor");
        
+//    }if (input == 2){
+//        finalString.push("Boop");
        
-   }else if (input == 2){
-       finalString.push("Boop");
-       
-   }else if (input == 1){
-       finalString.push("Beep");
+//    }if (input == 1){
+//        finalString.push("Beep");
      
-   }else {
-       finalString.push(i);
-   }
+//    }else{
+//     (finalString.push(i));
+//     console.log("hi");
+//    } 
+
+//    }
    
    
-}
-console.log(finalString);
-console.log(input)
+// }
 
-   }
 
+   //split input into array
+   //does include and push to new array
+ 
+
+//    for (i = 0; i <= input; i++){
+//        if (input.contains(3)){
+//            console.log("3")
+//        }else if (input.contains(2)){
+//            console.log("2")
+//        }
+
+//    }
 
 
 
 
 $(document).ready(function(){
+    var one = "1";
+    var two = "2";
+    var three = "3";
+    var count=[];
+    var finalString=[];
+
 
     $("#form").submit(function(event){
         event.preventDefault();
+        var input = ($("#input").val());
+        var finalString=[];
+        
 
 
+        for (i = 0; i <= input; i++){
+          count.push(i)
+     
+        }
+         console.log(count);
       
+
+
+
+       count.forEach(function(number){
+           if (one.includes(number)){
+               number = "Beep!";
+               finalString.push(number);
+           }else if (two.includes(number)){
+               number ="Boop";
+               finalString.push(number);
+           }else {
+            finalString.push(number);
+
+           }
+       })
        
+       console.log(finalString);
         
-        var input = parseInt($("input#input").val());
-        calc(input,finalString );
+        // if (input.includes(3)){
+        //     console.log("3")
+
+        // }else if (input.includes(2)){
+        // finalString.push("beep")
+
+        // } else {
+        //     finalString.push(i)
+           
+        // }
         
         
         
+    
+
     })
  
-
 
 
 
