dde03df6ef401ab8148345bdc6ec0d1499c0734f
Steven Ly <1stevenly92@gmail.com>
2020-04-03 09:50:09 -0700

    got it to console log range from 0 to users inputted numbers

--- js/script.js
+++ js/script.js
@@ -1,11 +1,52 @@
+
+   var input;
+   var finalString=[ ]
+
+
+   function calc(input, finalString){
+   for (i = 0 ; i <= input; i++){
+ 
+    if (input == 3){
+       finalString.push("neighbor");
+       
+       
+   }else if (input == 2){
+       finalString.push("Boop");
+       
+   }else if (input == 1){
+       finalString.push("Beep");
+     
+   }else {
+       finalString.push(i);
+   }
+   
+   
+}
+console.log(finalString);
+console.log(input)
+
+   }
+
+
+
+
+
 $(document).ready(function(){
 
     $("#form").submit(function(event){
         event.preventDefault();
-        console.log("hello")
+
+
+      
+       
+        
+        var input = parseInt($("input#input").val());
+        calc(input,finalString );
+        
+        
+        
     })
-
-
+ 
 
 
 
