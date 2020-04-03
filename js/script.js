3e5342b9d7c661d7189948b26b6910578df74b85
Steven Ly <1stevenly92@gmail.com>
2020-04-03 11:32:31 -0700

    updated readme

--- js/script.js
+++ js/script.js
@@ -1,46 +1,4 @@
 
-//    var input;
-//    var finalString=[ ]
-
-//    function calc(input, finalString){
-//    for (i = 0 ; i <= input; i++){
- 
-//     if (input == 3){
-//        finalString.push("neighbor");
-       
-//    }if (input == 2){
-//        finalString.push("Boop");
-       
-//    }if (input == 1){
-//        finalString.push("Beep");
-     
-//    }else{
-//     (finalString.push(i));
-//     console.log("hi");
-//    } 
-
-//    }
-   
-   
-// }
-
-
-   //split input into array
-   //does include and push to new array
- 
-
-//    for (i = 0; i <= input; i++){
-//        if (input.contains(3)){
-//            console.log("3")
-//        }else if (input.contains(2)){
-//            console.log("2")
-//        }
-
-//    }
-
-
-
-
 $(document).ready(function(){
     var one = "1";
     var two = "2";
@@ -53,43 +11,35 @@ $(document).ready(function(){
         event.preventDefault();
         var input = ($("#input").val());
         var finalString=[];
+        countString = count.toString();
+        countSplit = countString.split("");
+        
         
 
 
         for (i = 0; i <= input; i++){
           count.push(i)
-     
         }
-         console.log(count);
-      
-
-
+        
 
        count.forEach(function(number){
-           if (one.includes(number)){
-               number = "Beep!";
+           if (three.includes(number)){
+               number = "Wont you be my neighbor?";
                finalString.push(number);
            }else if (two.includes(number)){
                number ="Boop";
                finalString.push(number);
-           }else {
+           }else if (one.includes(number)){
+               number="Beep"
+               finalString.push(number);
+           }
+           else {
             finalString.push(number);
 
            }
        })
        
-       console.log(finalString);
-        
-        // if (input.includes(3)){
-        //     console.log("3")
-
-        // }else if (input.includes(2)){
-        // finalString.push("beep")
-
-        // } else {
-        //     finalString.push(i)
-           
-        // }
+        console.log(finalString);
         
         
         
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Mr Robger's neighborhood
+
+
+## By Steven Ly - April 3, 2020
+
+## Description
+An application which takes user input and displays an output
+
+## Setup/Installation Requirements
+Clone this repository.
+
+Navigate to index.html file through your file interface.
+
+Right click and open in the browser of your choice.
+
+## Known Bugs
+No known bugs at this time.
+
+## Support and contact details
+If you have any suggestions or questions please email me at stevenly369@gmail.com
+
+## Technologies Used
+HTML CSS Git and GitHub JavaScript Bootstrap jQuery
+
+## License
+This software is licensed under the MIT license.
+
+Copyright (c) 2020 Steven Ly
